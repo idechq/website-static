77407305c67b73631be319ab7da11d1c5de4b565
Yinan Ren <44737507+18010032728@users.noreply.github.com>
2023-01-17 22:52:10 +0800

    Update showTeamInfo.js

--- js/showTeamInfo.js
+++ js/showTeamInfo.js
@@ -1,48 +1,68 @@
 var teamInfo = {
-  "UOregon": {
-    "name": "UOregon",
+  "Team_GEnoM_-_iGEM_IIT_MADRAS": {
+    "name": "GEnoM - iGEM IIT Madras",
     "reportID" : "000000",
   },
-  "ED-NAU": {
-    "name": "ED_NAU",
+  "NAU-CHINA-DE": {
+    "name": "NAU-CHINA-DE",
     "reportID" : "000001",
-  },
-  "OUC": {
-    "name": "OUC",
-    "reportID" : "000002",
   },
   "NEFU_China": {
     "name": "NEFU_China",
+    "reportID" : "000002",
+  },
+  "USTC": {
+    "name": "USTC",
     "reportID" : "000003",
   },
-  "CSU_CHINA": {
-    "name": "CSU_CHINA",
-    "reportID" : "000004",
+  "HUST-China": {
+    "name": "HUST-China",
+    "reportID" : "000003",
+  },
+  "SUSTech_Shenzhen": {
+    "name": "SUSTech_Shenzhen_2022",
+    "reportID" : "000005",
+  },
+  "VE_CPU": {
+    "name": "VE-CPU",
+    "reportID" : "000006",
+  },
+  "Edinburgh-UHAS_Ghana": {
+    "name": "Edinburgh-UHAS_Ghana",
+    "reportID": "000007",
+  },
+  "OUC_China": {
+    "name": "OUC_China",
+    "reportID" : "000008",
   },
   "OUC-Marine_Drugs": {
     "name": "OUC-Marine Drugs",
-    "reportID" : "000005",
+    "reportID" : "000009",
   },
-  "Vilnius_GMC": {
-    "name": "Vilnius GMC",
-    "reportID" : "000006",
+  "CSU_CHINA": {
+    "name": "CSU_CHINA2022",
+    "reportID" : "000009",
   },
-  "Edinburgh": {
-    "name": "Edinburgh",
-    "reportID": "000007",
+  "Team_GEnoM_-_iGEM_IIT_MADRAS": {
+    "name": "Team GEnoM - iGEM IIT MADRAS",
+    "reportID" : "000009",
   },
-  "CPU_CHINA_2021": {
-    "name": "CPU_CHINA 2021",
-    "reportID" : "000008",
+  "Tongji_China": {
+    "name": "Tongji_China",
+    "reportID" : "000009",
   },
-  "Imperial": {
-    "name": "Imperial",
+  "CPU_CHINA": {
+    "name": "CPU_CHINA",
+    "reportID" : "000009",
+  },
+  "LZU-CHINA": {
+    "name": "LZU-CHINA",
     "reportID" : "000009",
   },
 };
 
 function updateTeamInfoModal(id) {
   document.querySelector('#teamInfoModalLabel').innerHTML = teamInfo[id].name;
-  document.querySelector('#teamWikiLink').setAttribute('href', `https://idec2021.github.io/${id}`);
-  document.querySelector('#teamReportLink').setAttribute('href', `https://arxiv.idec.io/article/${teamInfo[id].reportID}`);
+  document.querySelector('#teamWikiLink').setAttribute('href', `https://idec-teams.github.io/2022_${id}`);
+  /**document.querySelector('#teamReportLink').setAttribute('href', `https://arxiv.idec.io/article/${teamInfo[id].reportID}`);**/
 }
